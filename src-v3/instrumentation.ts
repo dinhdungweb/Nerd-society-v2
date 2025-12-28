5b3dde60417580fd117a73746ea60c4497902f33
Dinhdungweb <dungmaster7@gmail.com>
2025-12-28 15:23:01 +0700

    chore: remove src-v3 template folder from git tracking

--- src-v3/instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-    // Only run on server side
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
-        const { initCronJobs } = await import('./lib/cron')
-        initCronJobs()
-    }
-}
