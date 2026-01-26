603123ed76d8e17f1b8f1eae1ce62c49e0617d8d
dinhdungweb <dungmaster7@gmail.com>
2026-01-26 12:29:31 +0700

    Fix ButtonClose TS error

--- src/shared/ButtonClose.tsx
+++ src/shared/ButtonClose.tsx
@@ -2,7 +2,7 @@ import { Cancel01Icon } from '@hugeicons/core-free-icons'
 import { HugeiconsIcon } from '@hugeicons/react'
 import { ButtonCircleProps, ButtonCircle as ButtonCircleUI } from './Button'
 
-const ButtonClose = ({ className, ...props }: ButtonCircleProps & {}) => {
+const ButtonClose = ({ className, color, outline, plain, ...props }: ButtonCircleProps & {}) => {
   return (
     <ButtonCircleUI plain {...props} className={className}>
       <span className="sr-only">close</span>
