056a9b56fa53a90b7e788b4a0d0fef06e9ad1bf2
dinhdungweb <dungmaster7@gmail.com>
2026-01-29 08:56:40 +0700

    feat: add security headers

--- next.config.mjs
+++ next.config.mjs
@@ -39,6 +39,42 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self' https: data:;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https:;
+              style-src 'self' 'unsafe-inline' https:;
+              img-src 'self' blob: data: https:;
+              font-src 'self' data: https:;
+              connect-src 'self' https:;
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+      {
         // Disable caching for all admin pages
         source: '/admin/:path*',
         headers: [
