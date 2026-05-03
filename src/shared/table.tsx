40432671a646b8b2bc912ea9ee375be8a47ebef2
Dinhdungweb <dungmaster7@gmail.com>
2026-05-03 22:25:11 +0700

    Fix: Table first/last column padding too small (1 -> 6)

--- src/shared/table.tsx
+++ src/shared/table.tsx
@@ -84,7 +84,7 @@ export function TableHeader({ className, ...props }: React.ComponentPropsWithout
         className,
         'border-b border-b-neutral-950/10 px-4 py-2 font-medium first:pl-(--gutter,--spacing(2)) last:pr-(--gutter,--spacing(2)) dark:border-b-white/10',
         grid && 'border-l border-l-neutral-950/5 first:border-l-0 dark:border-l-white/5',
-        !bleed && 'sm:first:pl-1 sm:last:pr-1'
+        !bleed && 'sm:first:pl-6 sm:last:pr-6'
       )}
     />
   )
@@ -105,7 +105,7 @@ export function TableCell({ className, children, ...props }: React.ComponentProp
         !striped && 'border-b border-neutral-950/5 dark:border-white/5',
         grid && 'border-l border-l-neutral-950/5 first:border-l-0 dark:border-l-white/5',
         dense ? 'py-2.5' : 'py-4',
-        !bleed && 'sm:first:pl-1 sm:last:pr-1'
+        !bleed && 'sm:first:pl-6 sm:last:pr-6'
       )}
     >
       {href && (
